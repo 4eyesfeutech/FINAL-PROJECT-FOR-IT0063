3fc607234536846329f2bd6b44e45a4ed2908d2b
amandasmnte <amandanicolesamonte@gmail.com>
2025-03-24 14:16:07 +0800

    FINALS-STORE

---
+++ pages/store/script.js
@@ -0,0 +1,29 @@
+
+var popup = document.getElementById("popup");
+var span = document.getElementById("closePopup");
+
+function openPopup(title, price, imageUrl, description) {
+    document.getElementById("popupTitle").innerText = title;
+    document.getElementById("popupPrice").innerText = price;
+    document.getElementById("popupImage").src = imageUrl; 
+    document.getElementById("popupDescription").innerText = description; 
+    popup.style.display = "block";
+}
+
+document.getElementById('buyButton').addEventListener('click', function() {
+    const title = document.getElementById('popupTitle').innerText;
+    const price = document.getElementById('popupPrice').innerText;
+    alert(`You have purchased ${title} for ${price}!`);
+    
+});
+
+span.onclick = function() {
+    popup.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
